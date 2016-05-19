172e6cd84574f334c106b91bf2019569623970ec
VadVadim <Vadim89-89@inbox.ru>
2016-05-19 07:21:03 +0300

    Spring MVC Application was created

---
+++ src/java/controller/ActproController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+import service.ActproService;
+
+public class ActproController extends SimpleFormController {
+
+    private ActproService actproService;
+
+    public ActproController() {
+        setCommandClass(Name.class);
+        setCommandName("name");
+        setSuccessView("helloView");
+        setFormView("nameView");
+    }
+
+    @Override
+    protected ModelAndView onSubmit(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object command,
+            BindException errors) throws Exception {
+
+        Name name = (Name) command;
+        ModelAndView mv = new ModelAndView(getSuccessView());
+        mv.addObject("helloMessage", actproService.sayHello(name.getValue()));
+        return mv;
+    }
+
+    public void setActproService(ActproService actproService) {
+        this.actproService = actproService;
+    }
+}
---
+++ src/java/controller/Name.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+public class Name {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
